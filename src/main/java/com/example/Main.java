6bda7f98266cc6e5bbea71091d7b26c61e00f2d5
Mnunez1001 <mnunezpalomares@guilford.edu>
2025-01-08 10:00:24 -0600

    zero commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //add a new println statement that says "Hello, Mr. Rob"
+        System.out.println("Hello, Mr. Rob");
+
+        // add a new int statement that declares a variable called "myNumber" 
+        //and assigns it the value 5
+        int myNumber = 5;
+    
+    }
+}
